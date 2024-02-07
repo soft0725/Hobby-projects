d5a35a81ff1b2734166a4ddfe1d286db44695d5c
soft0725 <byungchan0725@gmail.com>
2024-02-07 12:23:43 +0900

    조회 코드 수정

--- Schedule management Discordbot/Commands/MeetingCommand.py
+++ Schedule management Discordbot/Commands/MeetingCommand.py
@@ -3,21 +3,24 @@ import re
 from discord.ext import commands
 
 from DataBase.upload_database import upload
+from DataBase.view_database import select_all, select_user
 
 class Meeting(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    def show_meeting_list_user(self): # 모든 스케줄 목록 
-        pass
+    @commands.command()
+    async def 전체일정(self, ctx): # 모든 스케줄 목록 
+        await select_all(ctx.message)
 
-    def show_meeting_list_all(self): # 특정 유저의 스케줄 목록 
-        pass
+    @commands.command()
+    async def 나의일정(self, ctx): # 특정 유저의 스케줄 목록  
+        await select_user(ctx.message)
 
-    def upload_database(self, match, userid): # 데이터베이스 스케줄 업로드 
+    def upload_database(self, match, userid): # 데이터베이스에 스케줄 업로드 
         upload(match, userid)
 
-    def delete_database(self): # 데이터베이스 스케쥴 삭제
+    def delete_database(self): # 데이터베이스 스케줄 삭제
         pass
 
     @commands.Cog.listener()  
@@ -29,7 +32,7 @@ class Meeting(commands.Cog):
             pattern = r'(오늘|내일|\d{4}년 \d{1,2}월 \d{1,2}일)\s*(오전|오후)?\s*(\d{1,2})시\s*(\d{1,2})분\s*(.+)?'
             match = re.match(pattern, message.content)
             if match: # 임베드 생성 
-                embed = discord.Embed(title="회의 정보", color=discord.Color.blue())
+                embed = discord.Embed(title="회의가 등록되었습니다.", color=discord.Color.blue())
                 embed.add_field(name="회의 이름", value=match.group(5) if match.group(5) else "없음", inline=False)
                 embed.add_field(name="날짜", value=match.group(1), inline=False)
                 embed.add_field(name="오전/오후", value=match.group(2))
